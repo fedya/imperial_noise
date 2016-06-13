3e6e451e5369b8d02efa83661fc8c66c5596c216
Alexander Stefanov-Khryukin <alexander@mezon.ru>
2016-06-13 19:30:49 +0300

    init

---
+++ purefy.sh
@@ -0,0 +1,34 @@
+#!/bin/sh                                                                                                                                
+
+# remove noise from AUDIO files
+input_file="$@"
+PATH="$PATH:/usr/bin:/usr/sbin"
+
+SOX=/usr/bin/sox
+
+remove_noise() {
+	echo "[make spectrogram for $input_file]"
+	$SOX $input_file -n spectrogram	-o $input_file.png
+	echo "[get noise sample from $input_file]"
+	# sometimes you need to change trim distance
+	$SOX $input_file noise.wav trim 0 00:01
+	#$SOX $input_file noise.wav trim 1 1.4
+	echo "[generate noise profile]"
+	$SOX noise.wav -n noiseprof $input_file.profile
+	echo "[cleanup $input_file]"
+	$SOX $input_file "$input_file"_cleaned.wav noisered $input_file.profile 0.21
+	echo "[make spectrogram for cleaned audio]"
+	$SOX "$input_file"_cleaned.wav -n spectrogram -o "$input_file"_cleaned.png
+	echo '[cleaned file placed in '$input_file'_cleaned.wav]'
+	echo "[cleanup all other noise]"
+	$SOX "$input_file"_cleaned.wav -r 8000 -c2  lowpass 4000 rate
+	echo "[wav to mp3 convertation, need installed lame codec]"
+	ffmpeg -i "$input_file"_cleaned.wav -vn -ar 44100 -ac 2 -ab 192 -f mp3 "$input_file"_cleaned.mp3
+}
+
+if [ $# -lt 1 ]; then
+  echo "Usage: $0 input_file"
+  exit 1
+fi
+
+remove_noise
