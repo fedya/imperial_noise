15c23e12cde72d60b3c95ed21a1ccdcf82ec6f5e
Alexander Khryukin <alexander@mezon.ru>
2016-06-13 19:41:04 +0300

    cleanup

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # imperial_noise
 Remove noise from old Imperial Russia romances.
+
+This script writter for https://vk.com/club106607447 and helps to remove noise and all other odd stuff
+from old Russian romances.
+
+
--- purefy.sh
+++ purefy.sh
@@ -11,8 +11,8 @@ remove_noise() {
 	$SOX $input_file -n spectrogram	-o $input_file.png
 	echo "[get noise sample from $input_file]"
 	# sometimes you need to change trim distance
-	$SOX $input_file noise.wav trim 0 00:01
-	#$SOX $input_file noise.wav trim 1 1.4
+	#$SOX $input_file noise.wav trim 0 00:01
+	$SOX $input_file noise.wav trim 1 1.2
 	echo "[generate noise profile]"
 	$SOX noise.wav -n noiseprof $input_file.profile
 	echo "[cleanup $input_file]"
@@ -21,9 +21,10 @@ remove_noise() {
 	$SOX "$input_file"_cleaned.wav -n spectrogram -o "$input_file"_cleaned.png
 	echo '[cleaned file placed in '$input_file'_cleaned.wav]'
 	echo "[cleanup all other noise]"
-	$SOX "$input_file"_cleaned.wav -r 8000 -c2  lowpass 4000 rate
+	$SOX "$input_file"_cleaned.wav -r 8000 -c2 /tmp/output.wav  lowpass 4000 rate
 	echo "[wav to mp3 convertation, need installed lame codec]"
-	ffmpeg -i "$input_file"_cleaned.wav -vn -ar 44100 -ac 2 -ab 192 -f mp3 "$input_file"_cleaned.mp3
+	ffmpeg -i /tmp/output.wav -vn -ar 44100 -ac 2 -ab 192 -f mp3 "$input_file"_cleaned.mp3
+	rm -f /tmp/output.wav
 }
 
 if [ $# -lt 1 ]; then
